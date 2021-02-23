41762eae6482d82b85e9adecfb560dfa4a76474e
Alecks-Derevyanko <71339667+Alecks-Derevyanko@users.noreply.github.com>
2021-02-23 21:29:11 +0200

    1

--- js/task7.js
+++
@@ -1,7 +0,0 @@
-const rangeRef = document.querySelector('#font-size-control');
-const spanRef = document.querySelector('#text');
-
-rangeRef.addEventListener('input', () => {
-  const textSize = rangeRef.value;
-  spanRef.style.fontSize = `${textSize}px`;
-});
